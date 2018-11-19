74f87eaff06ca741cf17ceff4b7b9d81210861bd
edfranceschini <edson.franceshini@gmail.com>
2018-11-19 00:05:54 -0200

    Added Review model Create of fields.py to create the data type IntergerRangeField

--- django_review/review/models.py
+++ django_review/review/models.py
@@ -3,6 +3,9 @@ from django.utils.text import gettext_lazy as _
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from .fields import IntegerRangeField
+
+
 
 
 class Profile(models.Model):
@@ -31,6 +34,25 @@ class Company(models.Model):
     class Meta:
         verbose_name_plural = _('Company')
         verbose_name = _('Companies')
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    company = models.ForeignKey(Company, on_delete = models.CASCADE)
+    title = models.CharField(_('Title'), max_length = 64, blank = False)
+    rating = IntegerRangeField(_('Rating'), blank = False, min = 1, max = 5)
+    summary = models.CharField(_('Summary'), max_length = 10240, blank = False)
+    ip_address = models.IPAddressField(_('Sender IP'), blank = True)
+    sub_date = models.DateTimeField(_('Submission Date'), auto_created = True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = _('Review')
+        verbose_name_plural = _('Reviews')
+    
+
 
 
 @receiver(post_save, sender = User)
